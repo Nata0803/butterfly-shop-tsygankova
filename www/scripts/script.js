7b0e7b2dd5f5c381face63e07dbbdd312f46584f
Nata Tsygankova <nataliesuperstar2002@gmail.com>
2024-11-14 14:16:25 +0300

    hw

--- www/scripts/script.js
+++ www/scripts/script.js
@@ -1,22 +1,59 @@
-$document.ready(function(){
+$(document).ready(function () {
+
+  let $menu = $('.js-menu')
+
+  $('.js-burger').on('click', function () {
+    $menu.slideToggle();
+  });
 
 
+  // accordion
   let prevAccordionBtn;
   let openClass = 'open';
 
+  $('.js-accordion-btn').on('click', function () {
+    if (this === prevAccordionBtn) {
+      $(this).toggleClass(openClass)
+      $(this).next().slideToggle();
+      return;
+    }
+
+    $(prevAccordionBtn).removeClass(openClass);
+    $(prevAccordionBtn).next().slideUp();
+
+    $(this).addClass(openClass);
+    $(this).next().slideDown();
+
+    prevAccordionBtn = this;
+  });
 
 
+  // slider
+
+  $('.js-slider').slick();
+
+
+  // tabs
+
+  $('.js-tab-link').on('click', function(event) {
+    event.preventDefault();
+
+    $('.js-tab-link').removeClass('.active');
+    $(this).addClass('.active');
+
+    let index = $(this).index('.js-tab-link');
+
+    $('.js-contacts-tab').removeClass('.active');
+    $('.js-contacts-tab').eq(index).addClass('.active');
   });
 
 
 
 
-
-
-
-
-
-
-
-
 });
+
+
+
+
+
+
